a61fbbd2ef880aaabda1075fb31c1b37ce3b2f95
John S. Marcial-Manjarres <59844100+jm01146@users.noreply.github.com>
2025-09-18 11:52:58 -0400

    Create Lab3_servo.ino

---
+++ Lab3_servo.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+   }
+   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+     myservo.write(pos);              // tell servo to go to position in variable 'pos'
+     delay(15);                       // waits 15 ms for the servo to reach the position
+   }                // Wait 250 milliseconds (OFF duration)
+}
